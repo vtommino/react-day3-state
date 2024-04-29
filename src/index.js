c9cf23175ff99888931e14aa00521ff066caacc1
Vasin Thepsoparn <vasindevwork@gmail.com>
2024-04-29 13:21:57 +0700

    cleanup:dependency

---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+
+  function App() {
+    return (
+      <div>
+        Hello React
+      </div>
+    );
+  }
+  
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(<App/>)
